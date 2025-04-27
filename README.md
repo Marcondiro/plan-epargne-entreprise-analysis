3836c38d6c0383e6874e6bb1daf5e963f1100168
Marcondiro <cavenati.marco@gmail.com>
2025-04-27 14:07:48 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Interessement and Plan Epargne Entreprise (PEE) analysis
+
+[Python notebook](./pee.ipynb) to simulate the placement of a "prime d'intéressement" in different PEE funds or in other saving forms.
---
+++ params_demo.py
@@ -0,0 +1,10 @@
+# Expected net taxable income for the year, excluding the prime d'intéressement (Revenu net imposable)
+REVENUS = 2730 * 12 # Average from https://www.insee.fr/fr/statistiques/7457170
+
+# prime d'intéressement
+PRIME = 2066 # Average from https://www.toutsurmesfinances.com/placements/prime-d-interessement-2018-2019-calcul-plafond-fiscalite-et-deblocage.html
+
+# Family quotient as defined by the french tax system
+QUOTIENT_FAMILIAL = 1
+
+YEARS = 5
